154e37ce9ff18b0216b2394d6d15f0204149d3fd
Raquel Teixeira <raquelat@ciandt.com>
2022-07-05 00:08:38 -0300

    spring boot

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_1;
+package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- 0x01/spring_boot_1/com/example/demo/SpringBoot1Application.java
+++ 0x01/spring_boot_1/com/example/demo/SpringBoot1Application.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_1;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
